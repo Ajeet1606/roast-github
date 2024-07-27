81712486128ff46b1ee35185037ffe2352df03bf
Ajeet Patel <patelajeet1606@gmail.com>
2024-07-27 18:34:01 +0530

    added github & twitter in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
 const Footer = () => {
   return (
     <div className="h-1/6 flex flex-col">
-      <h1 className="text-center text-sm p-5 mt-auto">
+      <div className="flex justify-center items-center gap-2 mb-2 mt-auto">
+        <a href="https://github.com/Ajeet1606/roast-github">
+          <img src="/src/assets/github.png" alt="github logo" className="w-6 mb-2" />
+        </a>
+        <a href="https://twitter.com/iampatelajeet">
+          <img src="/src/assets/twitter.png" alt="" className="w-5" />
+        </a>
+      </div>
+      <h1 className="text-center text-sm mb-2">
         Made with ❤️ by{" "}
         <span className="font-bold">
           <a href="https://twitter.com/iampatelajeet">Ajeet Patel</a>
